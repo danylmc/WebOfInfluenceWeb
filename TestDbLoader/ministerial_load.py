37ac8c3956c71fd25dcf13209047fd3ff1e5a74d
Savio Concessio <concessavi@myvuw.ac.nz>
2025-01-22 19:16:39 +1300

    candidate/minister retrieve/creator from Entities db

--- TestDbLoader/ministerial_load.py
+++ TestDbLoader/ministerial_load.py
@@ -10,4 +10,12 @@ connection = mysql.connector.connect(
     passwd = "root"
 )
 mycursor = connection.cursor()
-ld.use_db(mycursor, "Ministerial_")
+# ld.use_db(mycursor, "Ministerial_")
+
+def get_minister_id(minister_name_first, minister_name_last):
+    ld.use_db(mycursor, "Entities")
+    person_check = ld.check_tb(mycursor, (f"People WHERE first_name = \"{minister_name_first}\" AND last_name = \"{minister_name_last}\""))
+    if not person_check:
+        ld.import_data(connection, mycursor, "People", ("first_name", "last_name"), (minister_name_first.upper(), minister_name_last.upper()))
+    return ((ld.check_tb(mycursor, (f"People WHERE first_name = \"{minister_name_first}\" AND last_name = \"{minister_name_last}\"")))[0])
+    
