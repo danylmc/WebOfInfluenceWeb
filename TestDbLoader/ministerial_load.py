aca05a001598c46bbaad4c7793378b5fc1260a2b
Savio Concessio <concessavi@myvuw.ac.nz>
2025-01-24 11:53:49 +1300

    TIme and location normaliser to work for MD data format

--- TestDbLoader/ministerial_load.py
+++ TestDbLoader/ministerial_load.py
@@ -2,7 +2,8 @@ import mysql.connector
 from mysql.connector.errors import InterfaceError
 import loader as ld
 import pandas 
-
+import re
+from datetime import datetime
 
 connection = mysql.connector.connect(
     host="localhost",
@@ -11,6 +12,50 @@ connection = mysql.connector.connect(
 )
 mycursor = connection.cursor()
 # ld.use_db(mycursor, "Ministerial_")
+
+# Time normaliser 
+def get_time_tuple(time_string):
+    pattern = r"(\d{1,2}:\d{2}\s?[APM]{2})"
+    time_matches = re.findall(pattern, time_string)
+    
+    if not time_matches or len(time_matches) != 2:
+        raise ValueError("Time string not in correct format or does not contain exactly two times.")
+    
+    time_1200_format = []
+    
+    for time in time_matches:
+        # Ensure a space exists between the time and AM/PM
+        time = time.strip()
+        if time[-2:] not in ["AM", "PM"]:
+            time = time[:-2] + " " + time[-2:]
+        
+        try:
+            # Convert to 24-hour format and format as HHMM
+            time_obj = datetime.strptime(time, "%I:%M %p")
+            time_1200_format.append(time_obj.strftime("%H%M"))
+        except ValueError:
+            raise ValueError(f"Invalid time format: {time}")
+    
+    return tuple(time_1200_format)
+
+# Location normaliser (Updated at Andrew Hoggard)
+def get_location(location_string):
+    wellington_based = [
+        'Parliament Buildings', 
+        'Te Papa', 
+        'Government House', 
+        'Welington', 
+        'Wellingtgon', 
+        'Wellington', 
+        'Microsoft Teams Meeting',
+        'Teams Meeting', 
+        'Online', 
+        'Video Conference', 
+        'L1 Library meeting room; Room 013'
+    ]
+    if location_string in wellington_based:
+        return 'Wellington'
+    return wellington_based
 
 def get_minister_id(minister_name_first, minister_name_last):
     ld.use_db(mycursor, "Entities")
