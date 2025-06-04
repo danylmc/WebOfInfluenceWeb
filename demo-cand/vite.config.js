a41ac485fbeba2864663133566445fb3b45d0d72
kng-04 <nkrystal.2004@gmail.com>
2025-06-04 22:13:36 +1200

    edited vite.congif.js for ngrok server

--- demo-cand/vite.config.js
+++ demo-cand/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // needed to accept external connections
+    allowedHosts: [
+      '3327-49-224-81-158.ngrok-free.app' // 👈 your ngrok hostname
+    ]
+  }
 })
