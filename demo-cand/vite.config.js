f5816e96d9e392ecc306f8b49c74caecac4c1152
kng-04 <nkrystal.2004@gmail.com>
2025-07-22 19:53:28 +1200

    Apply local changes after rebase

--- demo-cand/vite.config.js
+++ demo-cand/vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+/* using ngrok free version */
+/* export default defineConfig({
   plugins: [react()],
   server: {
     host: true, // needed to accept external connections
     port: 5173,
     allowedHosts: ['.ngrok-free.app'] // ✅ Allow any hostname for ngrok
   }
+}) */
+
+/* using github pages */
+export default defineConfig({
+  base: '/WebOfInfluenceResearch/',
+  plugins: [react()],
 })
